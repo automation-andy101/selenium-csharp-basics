4b24703d05642b44a1b769480260545a0658b0a1
Andy Short <automation.andy101@gmail.com>
2023-03-04 08:59:33 +0000

    Added page objects modal

---
+++ csharp-nunit-seleniun/AutomationPracticePageObject.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.PageObjects;
+
+namespace csharp_nunit_seleniun
+{
+    class AutomationPracticePageObject
+    {
+        public AutomationPracticePageObject() 
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+
+        [FindsBy(How = How.Id, Using = "name")]
+        public IWebElement txtSwitchToAlertInput { get; set; }
+
+        [FindsBy(How = How.Name, Using = "dropdown-class-example")]
+        public IWebElement ddlExampleDropDown { get; set; }
+
+        [FindsBy(How = How.Id, Using = "checkBoxOption1")]
+        public IWebElement cbCheckboxExample { get; set; }
+    }
+}
---
+++ csharp-nunit-seleniun/TestClass3UsingPageObjects.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace csharp_nunit_seleniun
+{
+    internal class TestClass3UsingPageObjects
+    {
+
+
+        [SetUp]
+        public void Initialize()
+        {
+            PropertiesCollection.driver = new ChromeDriver();
+            PropertiesCollection.driver.Navigate().GoToUrl("https://rahulshettyacademy.com/AutomationPractice/");
+        }
+
+        [Test]
+        public void TestCustomMethods()
+        {
+            // Enter text into text input box
+            AutomationPracticePageObject page = new AutomationPracticePageObject();
+            page.txtSwitchToAlertInput.SendKeys("Some random text");
+            
+            
+
+            // Check that correct text appears in element
+            Console.WriteLine("The value in the Switch to alert Example input box is: " + page.txtSwitchToAlertInput.Text);
+
+            // Click a checkbox
+            page.cbCheckboxExample.Click();
+
+
+            // Select option1 from dropdown
+            SeleniumCustomSetMethods.SelectDropdown("dropdown-class-example", "Option1", PropertyType.Name);
+            page.ddlExampleDropDown.Click();
+            
+        }
+
+
+            [TearDown]
+        public void Clean()
+        {
+            PropertiesCollection.driver.Close();
+        }
+    }
+}
